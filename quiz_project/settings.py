40f2e1c02c90d26e2a43be7e5b195e9a22725f52
kurumihara <kuruminyanx3ko@gmail.com>
2021-05-15 21:22:52 +0900

    add allowed_hosts

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w1-6ryxy(v-j*if9z*zd*ao&h9d^qrqfwa&cgf!^@(gb$%q(_3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pharmaquiz.pythonanywhere.com']
 
 
 # Application definition
