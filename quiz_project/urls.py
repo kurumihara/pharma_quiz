9accf520d95892cb8aa944b99b8382728a3aeef0
kurumihara <kuruminyanx3ko@gmail.com>
2021-05-15 19:11:58 +0900

    modified urls.py

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', include('quiz.urls'))
+    url(r'^q/', include('quiz.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
